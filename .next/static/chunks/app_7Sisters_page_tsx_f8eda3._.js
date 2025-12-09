949a03bc7219e5b8d25b3449b4d69892a67fd03b
Dustin Mayfield-Jones <dustin@mayfieldjones.com>
2025-12-08 19:27:34 -0600

    Optimize homepage: prevent loading background image when video is present

---
+++ .next/static/chunks/app_7Sisters_page_tsx_f8eda3._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_7Sisters_page_tsx_f8eda3._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_7Sisters_page_tsx_f8eda3._.js",
+  "chunks": [
+    "static/chunks/app_7Sisters_index_aafbbd.css",
+    "static/chunks/app_7Sisters_670591._.js"
+  ],
+  "source": "dynamic"
+});
